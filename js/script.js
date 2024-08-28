ce2045971430d8f14e5cdad0de89fdeb66012be4
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2024-08-28 21:09:08 +0600

    counters have been completed

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+
+$(document).ready(function(){
+    //counter example-1
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    //counter example-2
+    $("#num1").countMe(40,65);
+    $("#num2").countMe(30, 30);
+    $("#num3").countMe(40, 50);
+    $("#num4").countMe(100,200);
+
+    //counter example-3
+    const timerElement = $('.timer');
+    timerElement.TickWatch({
+        direction: 'up',
+        startTime: '00:00:50',
+    })
+    
+
+    timerElement.on('TickWatch.end', function(){
+        alert('End!')
+    })
+
+    $('#stop').on('click', function () {
+        timerElement.TickWatch('stop');
+    });
+
+    $('#start').on('click', function () {
+        timerElement.TickWatch('start');
+    });
+
+    $('#clear').on('click', function () {
+        timerElement.TickWatch('clear');
+    });
+
+
+    //counter example-4
+    $("#countdown").countdown360({
+        radius      : 60,
+        seconds     : 100,
+        fontColor   : '#FFFFFF',
+        autostart   : false,
+        onComplete  : function () { console.log('done') }
+       }).start()
+
+
+
+   
+    
+
+      
+
+
+})
